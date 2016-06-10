4142099f02efbbc46869202db9ae788487471193
Eliot BARDET <eliot.bardet@gmail.com>
2016-06-10 18:39:16 +0200

    functional listing, filter and sort

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,63 @@
 class PlanetsController < ApplicationController
+  before_action :set_planet, only: %i(show destroy)
+
+  def index
+    planets = if planet_filter == 'giant'
+                Planet.giant
+              elsif planet_filter == 'terrestrial'
+                Planet.terrestrial
+              else
+                Planet.all
+              end
+
+    @planets = planets.order('planets.name ' + sort_direction)
+  end
+
+  def show
+  end
+
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.new(planet_params)
+
+    respond_to do |format|
+      if @planet.save
+        format.html { redirect_to planets_url }
+        format.json { render :show, status: :created, location: @planet }
+      else
+        format.html { render :new }
+        format.json { render json: @planet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @planet.destroy
+
+    respond_to do |format|
+      format.html { redirect_to planets_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_planet
+    @planet = Planet.find(params[:id])
+  end
+
+  def planet_params
+    params.require(:planet).permit(:name, :planet_type, :star_id)
+  end
+
+  def sort_direction
+    %w(ASC DESC).include?(params[:direction]) ? params[:direction] : 'ASC'
+  end
+
+  def planet_filter
+    params[:filter]
+  end
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -5,8 +5,11 @@ class Planet < ActiveRecord::Base
   validate :has_valid_type
   validates :star, presence: true
 
+  scope :terrestrial, -> { where(planet_type: 'Telluric') }
+  scope :giant, -> { where(planet_type: 'Gaseous') }
+
   def self.valid_types
-    ['Gaseous', 'Telluric']
+    %w(Gaseous Telluric)
   end
 
   private
